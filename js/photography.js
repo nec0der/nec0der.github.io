8b76af80afe8dd46bbfd92e02ed67c69e3f2564f
Emil Niyaz <niyaz.emil@gmail.com>
2020-04-02 18:25:52 -0400

    Add tab logo

--- js/photography.js
+++ js/photography.js
@@ -1,30 +1,5 @@
 $(document).ready(() => {
-  var loadingIndicator = $('.lds-dual-ring');
-  var folder = 'images/photography';
-  var rightPlace = $('#photo-wrapper');
-
-  // $.ajax({
-  //   url: folder,
-  //   headers: {
-  //     'Access-Control-Allow-Origin': '*'
-  //   },
-  //   success: function(data) {
-      loadingIndicator.toggle('lds-dual-ring');
-
-  //     $(data)
-  //       .find('a')
-  //       .attr('href', function(i, val) {
-  //         if (val.match(/\.(jpe?g|png|gif)$/)) {
-  //           let newPicture =
-  //             "<li><img class='photo lazy' data-src='" + val + "'></li>";
-
-  //           rightPlace.append(newPicture);
-
-            $(function() {
-              $('.lazy').lazy();
-            });
-  //         }
-  //       });
-  //   }
-  // });
+  $(function() {
+    $('.lazy').lazy();
+  });
 });
