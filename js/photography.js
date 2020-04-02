7c69263f0ec0d90e1345e6c9d903ba3d2475e225
Emil Niyaz <niyaz.emil@gmail.com>
2020-04-02 16:32:53 -0400

    Check first image

--- js/photography.js
+++ js/photography.js
@@ -3,28 +3,28 @@ $(document).ready(() => {
   var folder = 'images/photography';
   var rightPlace = $('#photo-wrapper');
 
-  $.ajax({
-    url: folder,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    success: function(data) {
+  // $.ajax({
+  //   url: folder,
+  //   headers: {
+  //     'Access-Control-Allow-Origin': '*'
+  //   },
+  //   success: function(data) {
       loadingIndicator.toggle('lds-dual-ring');
 
-      $(data)
-        .find('a')
-        .attr('href', function(i, val) {
-          if (val.match(/\.(jpe?g|png|gif)$/)) {
-            let newPicture =
-              "<li><img class='photo lazy' data-src='" + val + "'></li>";
+  //     $(data)
+  //       .find('a')
+  //       .attr('href', function(i, val) {
+  //         if (val.match(/\.(jpe?g|png|gif)$/)) {
+  //           let newPicture =
+  //             "<li><img class='photo lazy' data-src='" + val + "'></li>";
 
-            rightPlace.append(newPicture);
+  //           rightPlace.append(newPicture);
 
             $(function() {
               $('.lazy').lazy();
             });
-          }
-        });
-    }
-  });
+  //         }
+  //       });
+  //   }
+  // });
 });
